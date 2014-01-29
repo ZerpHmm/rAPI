20528dd9e84f1726a7a11be489f23c1c3cd140b3
rafoudiablol <rafoudiablol@hotmail.fr>
2014-01-29 19:53:04 +0100

    spaces fix

--- src/rafoudiablol/api/ench/EnchantmentManager.java
+++ src/rafoudiablol/api/ench/EnchantmentManager.java
@@ -12,53 +12,53 @@ import net.minecraftforge.client.IItemRenderer;
 
 public class EnchantmentManager
 {
-	/**
-	 *	@return true if something happened
-	**/
+    /**
+     * @return true if something happened
+     **/
 	
 	public boolean sendItemEventToEnchantments(ItemEvent event)
-	{
+    {
 		boolean somethingHappened = false;
-    	final ItemStack stack = event.stack;
-    	
+		final ItemStack stack = event.stack;
+	
 		if(stack.isItemEnchanted())
 		{
-			final Map<?, ?> enchantments = EnchantmentHelper.getEnchantments(stack);
-	    	
-			for(Map.Entry<?, ?> entry : enchantments.entrySet())
-			{
-				final int id = (Integer)entry.getKey();
-				final int lvl = (Integer)entry.getValue();
+		    final Map<?, ?> enchantments = EnchantmentHelper.getEnchantments(stack);
+	
+		    for (Map.Entry<?, ?> entry : enchantments.entrySet())
+		    {
+				final int id = (Integer) entry.getKey();
+				final int lvl = (Integer) entry.getValue();
 				final Enchantment enchantment = Enchantment.enchantmentsList[id];
-		    	
+		
 				if(enchantment instanceof IEnchantHandler)
 				{
-					if(processEventToHandler(id, lvl, event, (IEnchantHandler)enchantment)) {
-						somethingHappened = true;
-					}
+				    if(processEventToHandler(id, lvl, event, (IEnchantHandler) enchantment)) {
+					somethingHappened = true;
+				   }
 				}
-			}
+		    }
 		}
-		
+
 		return somethingHappened;
-	}
-	
-	protected boolean processEventToHandler(int id, int lvl, ItemEvent event, IEnchantHandler handler)
-	{
+    }
+
+    protected boolean processEventToHandler(int id, int lvl, ItemEvent event, IEnchantHandler handler)
+    {
 		if(event instanceof ItemEvents.BlockInteraction)
 		{
-			return handler.handleBlockInteraction(id, lvl, (ItemEvents.BlockInteraction)event);
+		    return handler.handleBlockInteraction(id, lvl, (ItemEvents.BlockInteraction) event);
 		}
 		else if(event instanceof ItemEvents.EntityInteraction)
 		{
-			return handler.handleEntityInteraction(id, lvl, (ItemEvents.EntityInteraction)event);
+		    return handler.handleEntityInteraction(id, lvl, (ItemEvents.EntityInteraction) event);
 		}
 		else if(event instanceof ItemEvents.RightClick)
 		{
-			return handler.handleRightClick(id, lvl, (ItemEvents.RightClick)event);
+		    return handler.handleRightClick(id, lvl, (ItemEvents.RightClick) event);
 		}
-		
+
 		// wtf ??
 		return false;
-	}
+    }
 }
